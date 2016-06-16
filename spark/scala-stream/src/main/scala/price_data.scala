adba4be0b9a6d292597666a1d1b3c2fa510bcb9b
Ubuntu <ubuntu@ip-172-31-0-199.us-west-2.compute.internal>
2016-06-16 17:06:26 +0000

    Added the distance metric

--- spark/scala-stream/src/main/scala/price_data.scala
+++ spark/scala-stream/src/main/scala/price_data.scala
@@ -6,10 +6,14 @@ import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkContext
 import org.apache.spark.sql._
+import scala.collection.mutable.{HashMap => MMap}
+import scala.collection.mutable.{ArrayBuffer => MBuffer}
+import scala.util.control.Breaks.break
+
 
 object PriceDataStreaming {
   def main(args: Array[String]) {
-
+    val test = MMap.empty[Int,Int]
     val brokers = "localhost:9092"
     val topics = "FX_test"
     val topicsSet = topics.split(",").toSet
@@ -55,12 +59,66 @@ object PriceDataStreaming {
   }
 }
 
+
+class DataPoint(timestamp : Long, duration : Int, anchorNum : Int){
+	var permutation = new Array[Int](anchorNum)
+	// need to call fillPermutation
+    def fillPermulation()  {
+
+	}
+}
+
+class Node(level : Int){
+    val hashValues = MMap.empty[Int,Node]
+    var pointArray = MBuffer.empty[DataPoint]
+}
+
+class AnchorContext(){
+    
+}
+
+class FXHashTree(maxelem : Int, maxdepth: Int){
+    val root = new Node(0)
+    
+    def put(dp : DataPoint){
+        this._put(root,dp,0)
+    }
+
+    def _put(cnode : Node, dp : DataPoint, level : Int) {
+        if (level == maxdepth){
+            cnode.pointArray += dp
+            return
+        }
+        var nextValue = dp.permutation(level)
+        val hashSize = cnode.hashValues.size
+        if (hashSize > 0){
+            if (cnode.hashValues.contains(nextValue)){
+                _put(cnode.hashValues(nextValue), dp, level+1)
+            } else {
+                cnode.hashValues(nextValue) = new Node(level)
+                cnode.hashValues(nextValue).pointArray += dp
+            }
+        } else if (cnode.pointArray.length < maxelem) {
+            cnode.pointArray += dp
+        } else {
+            cnode.hashValues(nextValue) = new Node(level)
+            cnode.pointArray.foreach((ii : DataPoint) => _put(cnode,ii,level))
+            cnode.pointArray = MBuffer.empty[DataPoint]
+        }
+    }
+
+}
+
+
+
+
 case class Tick(source: String, price: Double, volume: Int)
 
 
 
 
 /** Lazily instantiated singleton instance of SQLContext */
+
 
 object SQLContextSingleton {
 
